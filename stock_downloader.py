32683b97760d8f1bae077454cce5c1fd3a3da398
Fiza Fathima PP <fizappm0032@gmail.com>
2026-07-07 10:54:44 +0530

    phase 3 and 4

--- stock_downloader.py
+++ stock_downloader.py
@@ -3,7 +3,7 @@ print("Program started")
 import yfinance as yf
 import ta
 
-print("installed")
+print("packages installed successfully")
 
 # User input
 symbol = input("Enter company symbol (example: COALINDIA.NS): ")
@@ -39,65 +39,119 @@ else:
 
     # Keep only required columns
     data = data[["Date", "Open", "High", "Low", "Close", "Volume"]]
-print("\nDataset Information:\n")
-print(data.info())
 
-print("\nMissing Values:\n")
-print(data.isnull().sum())
+    print("\nDataset Information:\n")
+    print(data.info())
 
-print("\nStatistical Summary:\n")
-print(data.describe())
+    print("\nMissing Values:\n")
+    print(data.isnull().sum())
 
-print("\n✅ Clean Data:\n")
-print(data)
+    print("\nStatistical Summary:\n")
+    print(data.describe())
 
-# Remove duplicate rows
-data.drop_duplicates(inplace=True)
+    print("\n✅ Clean Data:\n")
+    print(data)
 
-# Fill missing values if any
-data.fillna(method='ffill', inplace=True)
+   # Remove duplicate rows
+    data.drop_duplicates(inplace=True)
 
-print("\nDataset Shape:")
-print(data.shape)
+   # Fill missing values if any
+    data = data.ffill()
 
-# Calculate SMA
-data['SMA20']=data['Close'].rolling(20).mean()
+    print("\nDataset Shape:")
+    print(data.shape)
 
-data['SMA50']=data['Close'].rolling(50).mean()
+   # Calculate SMA
+    data['SMA20']=data['Close'].rolling(20).mean()
+
+    data['SMA50']=data['Close'].rolling(50).mean()
 
 # Calculate EMA
-data['EMA20']=data['Close'].ewm(span=20).mean()
+    data['EMA20']=data['Close'].ewm(span=20).mean()
 
-data['EMA50']=data['Close'].ewm(span=50).mean()
+    data['EMA50']=data['Close'].ewm(span=50).mean()
 
 # Calculate RSI
-from ta.momentum import RSIIndicator
+    from ta.momentum import RSIIndicator
 
-rsi=RSIIndicator(close=data['Close'])
+    rsi=RSIIndicator(close=data['Close'])
 
-data['RSI']=rsi.rsi()
+    data['RSI']=rsi.rsi()
 
 # Calculate MACD
-from ta.trend import MACD
+    from ta.trend import MACD
 
-macd=MACD(data['Close'])
+    macd=MACD(data['Close'])
 
-data['MACD']=macd.macd()
+    data['MACD']=macd.macd()
 
 # Calculate Bollinger Bands
-from ta.volatility import BollingerBands
+    from ta.volatility import BollingerBands
 
-bb=BollingerBands(data['Close'])
+    bb=BollingerBands(data['Close'])
 
-data['Upper']=bb.bollinger_hband()
+    data['Upper']=bb.bollinger_hband()
 
-data['Lower']=bb.bollinger_lband()
+    data['Lower']=bb.bollinger_lband()
+
+    import matplotlib.pyplot as plt
+
+# Plotting closing price
+    plt.figure(figsize=(12,6))
+    plt.plot(data['Date'], data['Close'])
+    plt.title("Closing Price Trend")
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.grid(True)
+    plt.savefig("closing_price.png")
+    plt.show()
+# Plotting SMA20 and SMA50
+    plt.figure(figsize=(12,6))
+    plt.plot(data['Date'], data['Close'], label='Close')
+    plt.plot(data['Date'], data['SMA20'], label='SMA20')
+    plt.plot(data['Date'], data['SMA50'], label='SMA50')
+
+    plt.legend()
+    plt.title("Moving Averages")
+    plt.grid(True)
+    plt.savefig("sma_plot.png")
+
+    plt.show()
+# Plotting EMA20 and EMA50
+    plt.figure(figsize=(12,6))
+
+    plt.plot(data['Date'], data['Close'], label='Close')
+    plt.plot(data['Date'], data['EMA20'], label='EMA20')
+    plt.plot(data['Date'], data['EMA50'], label='EMA50')
+
+    plt.title("Exponential Moving Averages")
+    plt.legend()
+    plt.grid(True)
+
+    plt.show()
+
+ # Plotting RSI
+    plt.figure(figsize=(12,4))
+
+    plt.plot(data['Date'], data['RSI'])
+
+    plt.axhline(70, linestyle='--')
+    plt.axhline(30, linestyle='--')
+
+    plt.title("RSI Indicator")
+    plt.savefig("rsi_plot.png")
+
+    plt.show()
 
 #verify
-print(data.tail())
+    print("\nIndicators Check:\n")
+
+    print(
+data[['Close','SMA20','SMA50','EMA20','EMA50','RSI','MACD','Upper','Lower']].tail()
+)
 
 # Save CSV
-filename = symbol.replace(".", "_") + "_clean.csv"
-data.to_csv(filename, index=False)
+    filename = symbol.replace(".", "_") + "_clean.csv"
+    data.to_csv(filename, index=False)
 
-print(f"\n✅ CSV saved as {filename}")
+    print(f"\n✅ CSV saved as {filename}")
